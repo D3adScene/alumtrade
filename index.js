9231a4fbc2a0b2fff8ad9d5a88796b10b62d0231
Eli <eli@codex.co.il>
2020-11-09 21:27:31 +0200

    .

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import '@babel/polyfill';
+
+import App from './App';
+
+import 'antd/dist/antd.css';
+import './style.scss';
+
+// axios.defaults.baseURL = 'https://herolo-task-message-app.herokuapp.com/api';
+axios.defaults.baseURL = 'http://localhost:3000/api';
+
+ReactDOM.render(<App />, document.getElementById('root'));
